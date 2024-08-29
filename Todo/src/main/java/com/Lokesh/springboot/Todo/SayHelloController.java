80e7226f516783e8e8195aabc28cd71e775958b5
lokesh <lokeshkumaravel29@gmail.com>
2024-08-29 16:24:58 +0530

    SpringBoot

---
+++ Todo/src/main/java/com/Lokesh/springboot/Todo/SayHelloController.java
@@ -0,0 +1,36 @@
+package com.Lokesh.springboot.Todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String SayHello()
+    {
+        return "Hello";
+    }
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayhellohtml()
+    {
+        StringBuilder str = new StringBuilder();
+        str.append("<html");
+        str.append("<head>");
+        str.append("<title>MyFirstApp</title>");
+        str.append("</head");
+        str.append("<Body><p>Hello World</p></Body>");
+        str.append("</html");
+        return str.toString();
+    }
+
+    @RequestMapping("say-hello-jsp")
+    @ResponseBody
+    public String sayhellojsp()
+    {
+        return "Hello JSP!";
+    }
+}
