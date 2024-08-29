434f8a841c3ea175b70e6a73f767c5c4d64ce715
lokesh <lokeshkumaravel29@gmail.com>
2024-08-30 00:32:07 +0530

    Initial

--- Todo/src/main/java/com/Lokesh/springboot/Todo/LoginController.java
+++ Todo/src/main/java/com/Lokesh/springboot/Todo/LoginController.java
@@ -2,13 +2,9 @@ package com.Lokesh.springboot.Todo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
-import org.springframework.ui.Model;
 
 @Controller
 public class LoginController {
@@ -20,18 +16,12 @@ public class LoginController {
         return "Login";
     }
 
-    @PostMapping("login")
-    public String gotowelcome(@RequestParam String name, @RequestParam String password, Model model) {
+     @RequestMapping(value="login",method = RequestMethod.POST)
+    // @ResponseBody
+    public String gotowelcome(@RequestParam String name,@RequestParam String password,ModelMap model)
+    {
         System.out.println(name);
         System.out.println(password);
         return "Welcome";
     }
-    //  @RequestMapping(value="login",method = RequestMethod.POST)
-    // // @ResponseBody
-    // public String gotowelcome(@RequestParam String name,@RequestParam String password,ModelMap model)
-    // {
-    //     model.put("name",name);
-    //     model.put("password",password);
-    //     return "Welcome";
-    // }
 }
